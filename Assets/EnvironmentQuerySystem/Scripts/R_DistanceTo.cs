7c4debbd687c82ec730f7bbdb62179ae436d7862
Kieran Coppins <kierancoppins2000@gmail.com>
2022-08-24 21:51:43 +0100

    New Rule

---
+++ Assets/EnvironmentQuerySystem/Scripts/R_DistanceTo.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Environment Queries/Distance To")]
+public class R_DistanceTo : Rule
+{
+    enum Operators
+    {
+        LessThan,
+        GreaterThan,
+        EqualTo
+    }
+
+    [SerializeField] float distance;
+    [SerializeField] Operators op;
+
+    public override Dictionary<Vector2Int, float> Run(Dictionary<Vector2Int, float> tiles, GameObject caller)
+    {
+        Dictionary<Vector2Int, float> newTiles = new Dictionary<Vector2Int, float>();
+
+        tilemapController = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<TilemapController>();
+
+        Vector2Int target = GetTargetTile(caller);
+
+        foreach (var tile in tiles)
+        {
+            float d = Vector2Int.Distance(tile.Key, target);
+            if (Decision(d))
+                newTiles[tile.Key] = tiles[tile.Key] + scoreModifier;
+            else if (!ignoreFailedTiles)
+                newTiles[tile.Key] = tiles[tile.Key];
+        }
+
+        return newTiles;
+    }
+
+    public bool Decision(float d)
+    {
+        if (op == Operators.LessThan)
+            return d < distance;
+        else if (op == Operators.GreaterThan)
+            return d > distance;
+        else if (op == Operators.EqualTo)
+            return d == distance;
+        return false;
+    }
+}
